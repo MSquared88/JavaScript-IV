dfd37ed5102898228b8ce22bed891f7dae9080d1
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-07-25 15:01:54 -0500

    working on Lambda (classes)

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,44 @@
 // CODE here for your Lambda Classes
+
+class Person{
+    constructor(attrs){
+        this.name = attrs.name
+        this.age = attrs.age
+        this.location = attrs.location
+    }
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
+}
+
+class Instructor extends Person{
+    constructor(instructorAttrs){
+        super(instructorAttrs)
+        
+        this.specialty = instructorAttrs.specialty
+        this.favLanguage = instructorAttrs.favLanguage
+        this.catchPhrase = instructorAttrs.catchPhrase
+    }
+    demo(subject){
+        return `Today we are learning about ${subject}`
+    }
+    grade(student, subject){
+        return `${student.name} receives a perfect score on ${subject}`
+    }
+}
+
+class Student extends Person{
+    constructor(studentAttrs){
+        super(studentAttrs)
+        
+        this.previousBackground = studentAttrs.previousBackground
+        this.className = studentAttrs.className
+        this.favSubjects = studentAttrs.favSubjects
+    }
+    demo(subject){
+        return `Today we are learning about ${subject}`
+    }
+    grade(student, subject){
+        return `${student.name} receives a perfect score on ${subject}`
+    }
+}
