c1bbbe7dc2832bb3d5b8d157e84fd4b6c3172927
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-07-25 15:47:16 -0500

    done with objects now working on the console.logs for the methods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -2,9 +2,9 @@
 
 class Person{
     constructor(attrs){
-        this.name = attrs.name
-        this.age = attrs.age
-        this.location = attrs.location
+        this.name = attrs.name;
+        this.age = attrs.age;
+        this.location = attrs.location;
     }
     speak(){
         return `Hello my name is ${this.name}, I am from ${this.location}`
@@ -13,11 +13,11 @@ class Person{
 
 class Instructor extends Person{
     constructor(instructorAttrs){
-        super(instructorAttrs)
+        super(instructorAttrs);
         
-        this.specialty = instructorAttrs.specialty
-        this.favLanguage = instructorAttrs.favLanguage
-        this.catchPhrase = instructorAttrs.catchPhrase
+        this.specialty = instructorAttrs.specialty;
+        this.favLanguage = instructorAttrs.favLanguage;
+        this.catchPhrase = instructorAttrs.catchPhrase;
     }
     demo(subject){
         return `Today we are learning about ${subject}`
@@ -27,13 +27,13 @@ class Instructor extends Person{
     }
 }
 
-class Student extends Person{
+class Student extends Person {
     constructor(studentAttrs){
         super(studentAttrs)
         
-        this.previousBackground = studentAttrs.previousBackground
-        this.className = studentAttrs.className
-        this.favSubjects = studentAttrs.favSubjects
+        this.previousBackground = studentAttrs.previousBackground;
+        this.className = studentAttrs.className;
+        this.favSubjects = studentAttrs.favSubjects;
     }
     listsSubjects(){
         this.favSubjects.forEach(() => {
@@ -49,7 +49,88 @@ class Student extends Person{
     }
 }
 
+class ProjectManager extends Instructor {
+    constructor(projectManegerAttrs){
+        super(projectManegerAttrs);
 
-ProjectManagers have the following Methods:
-standUp a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
-debugsCode a method that takes in a student object and a subject and logs out {name} debugs {student.name}'s code on {subject}
+        this.gradClassName = projectManegerAttrs.gradClassName;
+        this.favInstructor = projectManegerAttrs.favInstructor;
+    }
+    standUp(slackChannel){
+        return `${this.name} announces to ${slackChannel}, @channel standy times!`
+    }
+    debugCode(studentObj, subject){
+       return `${this.name} debugs${studerObj.name}'s code on ${subject}` 
+    }
+}
+
+
+// Instructor Objects
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+
+const barney = new Instructor({
+    name: 'Barney',
+    location: 'Bedrock',
+    age: 40,
+    favLanguage: 'Python',
+    specialty: 'Back-end',
+    catchPhrase: `Hows it going fred`
+  });
+
+
+  
+//   Student Objects
+const matthew = new Student({
+    name: 'Matthew',
+    location: 'Texas',
+    age: 31,
+    previousBackground: 'Udemy',
+    className: 'Web22',
+    favSubjects: ['OOP', 'JS', 'LESS']
+  });
+
+  const bambam = new Student({
+    name: 'BamBam',
+    location: 'Bedrock',
+    age: 3,
+    previousBackground: 'Smashing things',
+    className: 'Rock22',
+    favSubjects: ['Smash', 'Squish', 'Hit']
+  });
+
+
+//   PM Objects
+
+
+const mack = new ProjectManager({
+    name: 'Mack',
+    location: 'Seattle',
+    age: '25?',
+    gradClassName: 'Web19',
+    favInstructor:  'Brit',
+});
+
+const dino  = new ProjectManager({
+    name: 'Dino',
+    location: 'Freds House',
+    age: 12,
+    gradClassName: 'Rock19',
+    favInstructor:  'Brit'
+});
+
+console.log(fred.speak())
+console.log(fred.demo("JavaSrcipt"))
+console.log(fred.grade(bambam, "CSS"))
+console.log(dino.speak())
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
